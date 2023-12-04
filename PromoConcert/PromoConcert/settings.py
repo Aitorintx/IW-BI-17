759ec2da9160c6cd3bced5b01517c7b6782caaec
saaraahdz <sara.hernandez@opendeusto.es>
2023-12-04 15:36:04 +0100

    i18n

--- PromoConcert/PromoConcert/settings.py
+++ PromoConcert/PromoConcert/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +50,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.locale.LocaleMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = "PromoConcert.urls"
@@ -67,6 +71,16 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+LANGUAGES = [
+    ('en', _('English')),
+    ('es', _('Spanish')),
+]
+
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
 ]
 
 WSGI_APPLICATION = "PromoConcert.wsgi.application"
--- PromoConcert/appPromoConcert/urls.py
+++ PromoConcert/appPromoConcert/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('promotores/', views.index_promotores, name='indexPromotor'),
     path('promotores/<int:promotor_id>/', views.show_promotores, name='promotorDetail'), 
     path('festivales/', views.index_festivales, name='festivales'),
@@ -13,6 +13,14 @@ urlpatterns = [
     path('interpretes/<int:interprete_id>/', views.show_interpretes, name='interpreteDetail'), 
     path('formularioInterprete/', views.add_interprete, name='addInterprete'),
     path('formularioFestival/', views.add_festival, name='addFestival'),
+]
+
+urlpatterns += i18n_patterns(
+    path('', include('appPromoConcert.urls')),
+)
+
+urlpatterns += [
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 if settings.DEBUG:
