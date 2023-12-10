5e2a994b422405c84cbb2aff38e34f98ecef66f6
Aitorintx <93260675+Aitorintx@users.noreply.github.com>
2023-12-10 21:13:51 +0100

    filtrado de promotores

---
+++ PromoConcert/static/admin/js/filtroPromotor.js
@@ -0,0 +1,19 @@
+document.getElementById('promotorDropdown').addEventListener('change', function() {
+    var selectedPromotorId = this.value;
+    if (selectedPromotorId) {
+        loadPromotorDetails(selectedPromotorId);
+    } else {
+        document.getElementById('promotorInfo').innerHTML = '';
+    }
+});
+
+function loadPromotorDetails(promotorId) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('promotorInfo').innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "/promotores/" + promotorId, true);
+    xhttp.send();
+}
